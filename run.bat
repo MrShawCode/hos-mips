a30c2db158e62c367003f00373ebf708869adc99
Zhiyuan Shao <zyshao.hust@gmail.com>
2016-11-29 17:07:48 +0800

    revised run.bat to make vivado an optional action.

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 CD /D debug
-call program.bat
-start cmd /C PUTTY.EXE -serial COM4 -sercfg 115200
+:call program.bat	%NOTE: this command will overwrite your soft MIPS-core with standard core. Make sure you have vivado in PATH before using it%
+start "" PUTTY.EXE -serial COM4 -sercfg 115200
 start cmd /C loadMIPSfpga.bat ../obj/kernel/ucore-kernel-initrd
-REM mips-sde-elf-gdb.exe ../obj/kernel/ucore-kernel-initrd -x startup-ucore.txt
+mips-sde-elf-gdb.exe ../obj/kernel/ucore-kernel-initrd -x startup-ucore.txt
 CD /D ..
 EXIT
