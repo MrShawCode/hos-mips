410921790f83d892f61095aaebed05fb99348049
Zhiyuan Shao <zyshao@hust.edu.cn>
2018-12-26 21:54:02 -0500

    small fix to run.bat due to the switch to MTI cross-compiler

--- run.bat
+++ run.bat
@@ -2,6 +2,6 @@ CD /D debug
 :call program.bat	%NOTE: this command will overwrite your soft MIPS-core with standard core. Make sure you have vivado in PATH before using it%
 start "" PUTTY.EXE -serial COM4 -sercfg 115200
 start cmd /C loadMIPSfpga.bat ../obj/kernel/ucore-kernel-initrd
-mips-sde-elf-gdb.exe ../obj/kernel/ucore-kernel-initrd -x startup-ucore.txt
+mips-mti-elf-gdb.exe ../obj/kernel/ucore-kernel-initrd -x startup-ucore.txt
 CD /D ..
 EXIT
